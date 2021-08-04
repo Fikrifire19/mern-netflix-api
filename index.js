b8118269f214b079132ec302e34c472773fb6f48
Fikri <muhamadfikri64@yahoo.co.id>
2021-08-04 22:22:40 +0700

    Step 3 - Register Route

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 }).then(() => {
     console.log('DB connection success!');
     }).catch((err) => {
         console.log(err);
-    });
+});
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(9900, () => {
     console.log('Backend is running!');
