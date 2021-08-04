5159226aebbc6f3cedce0e853728094559722c33
Fikri <muhamadfikri64@yahoo.co.id>
2021-08-04 21:01:13 +0700

    Step 1 - Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('DB connection success!');
+    }).catch((err) => {
+        console.log(err);
+    });
+
+app.listen(9900, () => {
+    console.log('Backend is running!');
+});
