73f653734cd1d9b684a42d9cf98e7c65868aa875
Fikri <muhamadfikri64@yahoo.co.id>
2021-08-05 21:35:52 +0700

    Step 3 - Login Route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // Register
 router.post("/register", async (req, res) => {
@@ -13,6 +14,31 @@ router.post("/register", async (req, res) => {
     try {
         const user = await newUser.save();
         res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Login
+router.post("/login", async (req,res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(401).json('Wrong password or email!');
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && res.status(401).json('Wrong password or email!');
+
+        const accessToken = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.SECRET_KEY,
+            { expiresIn: "5d" }
+        );
+
+        const {password, ...info} = user._doc;
+
+        res.status(201).json({ ...info, accessToken });
     } catch (err) {
         res.status(500).json(err);
     }
